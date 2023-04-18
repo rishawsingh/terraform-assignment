a605e9e2825ff1dea39deb2ecb15e7a75ef75195
Rishaw Kumar <68506932+rishawsingh@users.noreply.github.com>
2023-04-18 16:32:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,16 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+}
+
 variable "ami" {
   type    = string
   default = "ami-0aef57767f5404a35"
