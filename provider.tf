d8b651e908876b92757071dbe7a98b49670db12d
Rishaw Kumar <68506932+rishawsingh@users.noreply.github.com>
2023-04-18 16:31:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
-# AWS Provider
-provider "aws" {
-  region = var.region
-  # value for region can be changed from terraform.tfvars
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 2.0"
+    }
+  }
 }
 
+provider "aws" {
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
