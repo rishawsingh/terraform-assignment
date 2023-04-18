b04f54cc623a0631af5db9ad9f478cdd79d17027
Rishaw Kumar <68506932+rishawsingh@users.noreply.github.com>
2023-04-18 19:30:08 +0530

    Update main_assessment.tf

--- main_assessment.tf
+++ main_assessment.tf
@@ -1,23 +1,6 @@
-
-# Create VPC
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Create Public Subnet
-resource "aws_subnet" "public_subnet" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id     = aws_vpc.vpc.id
-  tags = {
-    Name = "public-subnet"
-  }
-}
-
 # Create Security Group for EC2 Instance
 resource "aws_security_group" "ec2_sg" {
   name_prefix = "ec2-sg-"
-  vpc_id      = aws_vpc.vpc.id
-
   ingress {
     from_port   = 22
     to_port     = 22
@@ -33,10 +16,26 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
+# Create EC2 instance and add phpMyAdmin deployment to the EC2 instance
+resource "aws_instance" "wordpress_instance" {
+  ami                    = var.ami
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  user_data              = <<-EOF
+      #!/bin/bash
+      sudo yum update -y
+      sudo amazon-linux-extras install -y docker
+      sudo service docker start
+      sudo usermod -aG docker ec2-user
+      sudo docker run --name wordpress -p 80:80 -p 443:443 -d wordpress
+      # Install phpMyAdmin
+      sudo docker run --name phpmyadmin -d --link wordpress_db:db -p 8080:80 phpmyadmin/phpmyadmin
+    EOF
+}
 # Create Security Group for RDS Instance
 resource "aws_security_group" "rds_sg" {
   name_prefix = "rds-sg-"
-  vpc_id      = aws_vpc.vpc.id
+
 
   ingress {
     from_port   = 3306
@@ -48,49 +47,26 @@ resource "aws_security_group" "rds_sg" {
 
 # Create RDS instance
 resource "aws_db_instance" "wordpress_db" {
-  allocated_storage    = 20
+  allocated_storage    = 10
   engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t4g.micro"
+  instance_class       = "db.t3.micro"
   name                 = var.name
   username             = var.username
   password             = var.password
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.mysql8.0"
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  skip_final_snapshot = true
+
 }
 
-# Create EC2 instance and add phpMyAdmin deployment to the EC2 instance
-resource "aws_instance" "wordpress_instance" {
-  ami                    = var.ami
-  instance_type          = "t4g.small"
-  subnet_id              = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  key_name               = "my-key-pair"
-  user_data              = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo amazon-linux-extras install -y docker
-    sudo service docker start
-    sudo usermod -aG docker ec2-user
-    sudo docker run --name wordpress -p 80:80 -p 443:443 -d wordpress
 
-    # Install phpMyAdmin
-    sudo docker run --name phpmyadmin -d --link wordpress_db:db -p 8080:80 phpmyadmin/phpmyadmin
-  EOF
-}
 
 # Create Elastic IP and associate it with EC2 instance
 resource "aws_eip" "wordpress_eip" {
   instance = aws_instance.wordpress_instance.id
 }
 
-# Rollback on error
-terraform {
-  on_failure = "rollback"
-}
-
-# Output of public IP of EC2 istance
-output "public_ip" {
-  value = aws_instance.wordpress_instance.public_ip
+# Associate the Elastic IP with the EC2 instance
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.wordpress_instance.id
+  allocation_id = aws_eip.wordpress_eip.id
 }
